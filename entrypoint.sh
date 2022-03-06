e05cdaefc663b781c68bfca1927df9a38831f7ee
Puneet Matharu <puneet.matharu@hotmail.co.uk>
2022-03-06 11:44:19 +0000

    Propagate the exit code of the cmake-format command.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,14 @@
 
 cd "$GITHUB_WORKSPACE" || exit
 
-find . \( -name '*.cmake' -o -name 'CMakeLists.txt' \) -exec cmake-format $* {} \;
+files=$(find . \( -name '*.cmake' -o -name 'CMakeLists.txt' \))
+
+for file in ${files}; do
+    echo "Processing file: ${file}"
+    cmake-format $* "${file}"
+    exit_code="$?"
+    if [ ${exit_code} -ne 0 ]; then
+        echo "cmake-format failed. Exiting with exit code ${exit_code}."
+        exit ${exit_code}
+    fi
+done
